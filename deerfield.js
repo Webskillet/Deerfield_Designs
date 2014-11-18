dacf7595d9720a39cb9ac5182841452991b59966
Jonathan Kissam <jk@webskillet.com>
2014-11-18 12:47:02 -0500

    Javascript update 1

--- deerfield.js
+++ deerfield.js
@@ -1,16 +1,33 @@
 $(document).ready(function(){
+	// add logged-in class if #admin-bar exists
+	if ($('#admin-bar').length) { $('body').addClass('logged-in');
+
+	// test to see if we're on the home page
+	var isHome = false;
+	var urlBase = window.location.origin + window.location.pathname;
+	if (window.location.pathname == '/Deerfield_Designs/Stores/Page/1000000/Homepage') {
+		$('body').addClass('home');
+		urlBase = '';
+		isHome = true;
+	}
+
 	// add custom menu
 	$('.header-nav > ul > li:not(#header-nav-products)').remove();
-	var menuHtml = '<li id="header-nav-who" class="tablink"><a href="#who">Who</a></li><li id="header-nav-what" class="tablink"><a href="#what">What</a></li><li id="header-nav-why" class="tablink"><a href="#why">Why</a></li><li id="header-nav-how" class="tablink"><a href="#how">How</a></li>';
+	$('li#header-nav-products a').html('Wares&nbsp;<span class="arrow-down"></span>');
+	var menuHtml = '<li id="header-nav-who" class="tablink"><a href="'+urlBase+'#who">Who</a></li>';
+	manuHtml += '<li id="header-nav-what" class="tablink"><a href="'+urlBase+'#what">What</a></li>';
+	manuHtml += '<li id="header-nav-why" class="tablink"><a href="'+urlBase+'#why">Why</a></li>';
+	manuHtml += '<li id="header-nav-how" class="tablink"><a href="'+urlBase+'#how">How</a></li>';
 	$('.header-nav > ul').prepend(menuHtml);
 
-	// tabs functionality
-	// tabs should be set up like this: <div id="who" class="tab"> [content] </div>
-	$('.tab').hide();
-	$('#who').show();
-	$('.tablink a').click(function(event){
-		event.preventDefault();
+	if (isHome) {
+		var openTab = $(window.location.hash).hasClass('tab') ? window.location.hash : '#who';
 		$('.tab').hide();
-		$($(this).attr('href')).show();
-	});
+		$('#who').show();
+		$('.tablink a').click(function(event){
+			event.preventDefault();
+			$('.tab').hide();
+			$($(this).attr('href')).show();
+		});
+	}
 });
