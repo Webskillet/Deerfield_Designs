c97cb519cd9f9fa99bd56958a3a1a181dd77f4b5
firestk <alec@firestick.me>
2014-11-18 10:30:39 -0500

    Small css and js changes

--- deerfield.js
+++ deerfield.js
@@ -1,11 +1,7 @@
 $(document).ready(function(){
 	// add custom menu
 	$('.header-nav > ul > li:not(#header-nav-products)').remove();
-	var menuHtml = '
-		<li id="header-nav-who tablink"><a href="#who">Who</a></li>
-		<li id="header-nav-what tablink"><a href="#what">What</a></li>
-		<li id="header-nav-why tablink"><a href="#why">Why</a></li>
-		<li id="header-nav-how tablink"><a href="#how">How</a></li>';
+	var menuHtml = '<li id="header-nav-who" class="tablink"><a href="#who">Who</a></li><li id="header-nav-what" class="tablink"><a href="#what">What</a></li><li id="header-nav-why" class="tablink"><a href="#why">Why</a></li><li id="header-nav-how" class="tablink"><a href="#how">How</a></li>';
 	$('.header-nav > ul').prepend(menuHtml);
 
 	// tabs functionality
