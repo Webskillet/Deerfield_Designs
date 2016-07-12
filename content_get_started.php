de15443be83aa1173b341d59100f5630b177034c
alec <alec@webskillet.com>
2016-07-12 16:20:05 -0400

    Initial HTML boilerplate for Get Started page.

---
+++ content_get_started.php
@@ -0,0 +1,15 @@
+<div id="dd-get-started-wrapper">
+  <div id="dd-gs-custom-bulk">
+    <h2>Placing custom bulk orders</h2>
+    <p>For custom orders of 20 or more items, you may proceed in one of these three ways:</p>
+    <ul>
+      <li>Choose Your Product &mdash; browse the apparel, accessories and promotional items in PRODUCTS &amp; choose your first item color and quantities</li>
+      <li>Upload Your Art &mdash; go to the <a href="">design studio</a> and upload your art file. You can go here first and choose your product to decorate from here.</li>
+      <li>Get a Quote &mdash; let us put your order together for you! Use the Get A Quote form to give us an idea of what you're to order and we'll handle everything.</li>
+  </div>
+  <div id="dd-gs-shopping">
+    <h2>Shopping</h2>
+    <p>Browse from our existing product lines...</p>
+    <a class="btn">Shop Now</a>
+  </div>
+</div>
