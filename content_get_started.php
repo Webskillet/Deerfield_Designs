efa262dd983ab957539d9e71022b1c0a0b7e26bb
ahillio <alec@firestick.me>
2016-08-02 15:22:03 -0400

    Update content_get_started.php.
    Use content from production.

--- content_get_started.php
+++ content_get_started.php
@@ -3,13 +3,13 @@
     <h2>Placing custom bulk orders</h2>
     <p>For custom orders of 20 or more items, you may proceed in one of these three ways:</p>
     <ul>
-      <li>Choose Your Product &mdash; browse the apparel, accessories and promotional items in PRODUCTS &amp; choose your first item color and quantities</li>
-      <li>Upload Your Art &mdash; go to the <a href="">design studio</a> and upload your art file. You can go here first and choose your product to decorate from here.</li>
-      <li>Get a Quote &mdash; let us put your order together for you! Use the Get A Quote form to give us an idea of what you're to order and we'll handle everything.</li>
-  </div>
+      <li><span style="font-weight: bold;">Choose Your Product</span> — <a href="/Deerfield_Designs/products">browse the apparel, accessories and promotional items</a> in PRODUCTS &amp; choose your first item color and quantities</li>
+      <li><span style="font-weight: bold;">Upload Your Art</span> — go to the <a href="/Deerfield_Designs/DesignStudio/Home">design studio</a> and upload your art file. You can go here first and choose your product to decorate from here.</li>
+      <li><span style="font-weight: bold;">Get a Quote</span> — let us put your order together for you! Use the <a href="#" data-quote-toggle="">Get A Quote</a> form to give us an idea of what you're to order and we'll handle everything.</li>
+  </ul></div>
   <div id="dd-gs-shopping">
     <h2>Shopping</h2>
     <p>Browse from our existing product lines...</p>
-    <a class="btn-primary btn">Shop Now</a>
+    <a class="btn-primary btn" href="/Deerfield_Designs/Stores/Page/1000019/Shop_Now">Shop Now</a>
   </div>
 </div>
