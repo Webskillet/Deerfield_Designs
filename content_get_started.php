40bc88d1aacfb95d10f36943661413eb73432fe4
ahillio <alec@firestick.me>
2016-08-02 15:28:30 -0400

    Add comments to content_get_started.php.

--- content_get_started.php
+++ content_get_started.php
@@ -7,10 +7,10 @@
       <li><span style="font-weight: bold;">Upload Your Art</span> — go to the <a href="/Deerfield_Designs/DesignStudio/Home">design studio</a> and upload your art file. You can go here first and choose your product to decorate from here.</li>
       <li><span style="font-weight: bold;">Get a Quote</span> — let us put your order together for you! Use the <a href="#" data-quote-toggle="">Get A Quote</a> form to give us an idea of what you're to order and we'll handle everything.</li>
     </ul>
-  </div>
+  </div><!-- end dd-gs-custom-bulk -->
   <div id="dd-gs-shopping">
     <h2>Shopping</h2>
     <p>Browse from our existing product lines...</p>
     <a class="btn-primary btn" href="/Deerfield_Designs/Stores/Page/1000019/Shop_Now">Shop Now</a>
-  </div>
-</div>
+  </div><!-- end dd-gs-shopping -->
+</div><!-- end dd-get-started-wrapper -->
