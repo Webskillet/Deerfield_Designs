79405da566017286cd8c534b79ad267e09276585
ahillio <alec@firestick.me>
2016-08-02 15:26:00 -0400

    Correct indents in content_get_started.php.

--- content_get_started.php
+++ content_get_started.php
@@ -6,7 +6,8 @@
       <li><span style="font-weight: bold;">Choose Your Product</span> — <a href="/Deerfield_Designs/products">browse the apparel, accessories and promotional items</a> in PRODUCTS &amp; choose your first item color and quantities</li>
       <li><span style="font-weight: bold;">Upload Your Art</span> — go to the <a href="/Deerfield_Designs/DesignStudio/Home">design studio</a> and upload your art file. You can go here first and choose your product to decorate from here.</li>
       <li><span style="font-weight: bold;">Get a Quote</span> — let us put your order together for you! Use the <a href="#" data-quote-toggle="">Get A Quote</a> form to give us an idea of what you're to order and we'll handle everything.</li>
-  </ul></div>
+    </ul>
+  </div>
   <div id="dd-gs-shopping">
     <h2>Shopping</h2>
     <p>Browse from our existing product lines...</p>
